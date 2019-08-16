c5d23e8a59bf362d30d58974d55a7e7c6c876194
retr0 <395637649@qq.com>
2019-08-16 21:49:04 +0800

    [feat]:should_return_level_1_and_ace_TWO_when_given_2D_2H_5S_6H_7S

--- src/main/java/Hand.java
+++ src/main/java/Hand.java
@@ -23,42 +23,47 @@ public class Hand {
     }
 
     public void calHandLevelAndAce() {
-        power.ace = cards.get(cards.size() - 1);
-        power.level = 0;
+        if (tryPair() != null) {
+            power = tryPair();
+        } else {
+            power.ace = cards.get(cards.size() - 1);
+            power.level = 0;
+        }
     }
 
-    private ArrayList<Map.Entry<CardNumber,Long>> countCards() {
-        Map<CardNumber,Long> cardNumberCounts = cards.stream().map(Card::getNumber)
-                .collect(Collectors.groupingBy(x -> x,Collectors.counting()));
-        ArrayList<Map.Entry<CardNumber,Long>> list = new ArrayList<>(cardNumberCounts.entrySet());
+    private ArrayList<Map.Entry<CardNumber, Long>> countCards() {
+        Map<CardNumber, Long> cardNumberCounts = cards.stream().map(Card::getNumber)
+                .collect(Collectors.groupingBy(x -> x, Collectors.counting()));
+        ArrayList<Map.Entry<CardNumber, Long>> list = new ArrayList<>(cardNumberCounts.entrySet());
         list.sort(comparing(Map.Entry::getValue));
         return list;
     }
+
     public Power tryPair() {
-        ArrayList<Map.Entry<CardNumber,Long>> list = countCards();
-        final Map.Entry<CardNumber,Long> item = list.get(list.size() - 1);
+        ArrayList<Map.Entry<CardNumber, Long>> list = countCards();
+        final Map.Entry<CardNumber, Long> item = list.get(list.size() - 1);
         if (item.getValue() == 2L) {
-            return new Power(new Card(CardType.SPAED,item.getKey()),1);
+            return new Power(new Card(CardType.SPAED, item.getKey()), 1);
         }
         return null;
     }
 
     public Power tryTwoPairs() {
-        ArrayList<Map.Entry<CardNumber,Long>> list = countCards();
-        final Map.Entry<CardNumber,Long> item = list.get(list.size() - 1);
-        final Map.Entry<CardNumber,Long> secondItem = list.get(list.size() - 2);
-        final Map.Entry<CardNumber,Long> aceItem = item.getKey().compareTo(secondItem.getKey()) < 0 ? secondItem : item;
+        ArrayList<Map.Entry<CardNumber, Long>> list = countCards();
+        final Map.Entry<CardNumber, Long> item = list.get(list.size() - 1);
+        final Map.Entry<CardNumber, Long> secondItem = list.get(list.size() - 2);
+        final Map.Entry<CardNumber, Long> aceItem = item.getKey().compareTo(secondItem.getKey()) < 0 ? secondItem : item;
         if (item.getValue() == 2L && secondItem.getValue() == 2L) {
-            return new Power(new Card(CardType.SPAED,aceItem.getKey()),2);
+            return new Power(new Card(CardType.SPAED, aceItem.getKey()), 2);
         }
         return null;
     }
 
     public Power tryThreeOfAKind() {
-        ArrayList<Map.Entry<CardNumber,Long>> list = countCards();
-        final Map.Entry<CardNumber,Long> item = list.get(list.size() - 1);
+        ArrayList<Map.Entry<CardNumber, Long>> list = countCards();
+        final Map.Entry<CardNumber, Long> item = list.get(list.size() - 1);
         if (item.getValue() == 3L) {
-            return new Power(new Card(CardType.SPAED,item.getKey()),3);
+            return new Power(new Card(CardType.SPAED, item.getKey()), 3);
         }
         return null;
     }
